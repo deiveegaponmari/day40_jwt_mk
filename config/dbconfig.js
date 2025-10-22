e4f733d937b7358ea915552ec50638d646b16cad
deiveegaponmari <ponmarimdeiveega@gmail.com>
2025-10-22 14:04:56 +0530

    database connect now

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 const connectdb = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGO_URI
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Database connected successfully");
   } catch (error) {
     console.log("error:Database connection Failed");
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 require('dotenv').config();
+const connectdb=require("./config/dbconfig")
 const app=express();
 //middleware
 app.use(bodyparser.json());
 const port=process.env.PORT || 4000;
+//database connection
+connectdb();
 app.listen(port,()=>{
     console.log(`server is running successfully https://localhost:${port}`)
 })
